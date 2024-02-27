20355bf2885e48c51089ba6871642efbfc66f212
Michael Love <Michael.t.love3@gmail.com>
2024-02-26 21:07:10 -0600

    Was able to successfully test out pulling information and creating a character within the main program without causing any type of loopback errors.

--- Game.py
+++ Game.py
@@ -1,4 +1,18 @@
 import GameMenu
+import Character
+import Races
+import character_Classes
 
-while True:
-    GameMenu.Menu()
+
+
+# while True:
+#     GameMenu.Menu()
+
+# verified that information is able to be pulled correctly from associated files in this format.
+hero = Character.Character(
+    name='Michael',
+    race=Races.human,
+    characterClass=character_Classes.fighter
+)
+
+Character.statBlock(hero)
